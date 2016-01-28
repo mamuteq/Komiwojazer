232d1a24ca486049a50a4203cc0796dedb44f71e
Tomasz Czołba <tcmamut@gmail.com>
2016-01-28 09:20:09 +0100

    Add parameters and function

--- Komiwojazer/Komiwojazer.cpp
+++ Komiwojazer/Komiwojazer.cpp
@@ -2,7 +2,28 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <math.h>
 
+#define max_Tops 50 
+
+using namespace std;
+
+struct edge{
+	int top, lenght;
+};
+
+int lenght_Ham = INT_MAX,
+lenght = 0,
+top_Start;
+
+vector <edge> L[max_Tops + 1];
+vector <int> cyckle_Ham, cyckle;
+
+bool visited[max_Tops + 1];
+ifstream file;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
